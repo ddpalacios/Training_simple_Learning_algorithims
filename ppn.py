6b0ba50c0dd5b5d5bccf31020efaa82b8f8b6119
Daniel <palaciosdanieldario@gmail.com>
2019-10-03 13:35:10 -0500

    Set up variables to keep track during training

--- ppn.py
+++ ppn.py
@@ -21,7 +21,17 @@ class Perceptron(object):
 		self.lr =lr
 		self.epochs = epochs
 	def fit(self, X,y):
-		pass
+		self.weights = np.random.rand(X.shape[1]+ 1)  #Creating our starting weights with a bias
+		np.seed(0) #To make sure there are no different weight values
+
+		self.cost = []  #We need to keep track of our learning expenses so we can graph later
+
+
+		for _ in range(self.epochs):
+			error = 0 #Amount of errors per epochs
+
+			for xi, target in zip(X,y):
+				update = self.lr * (self.predict(xi))
 
 	def net_input(self,X):
 		pass
@@ -29,8 +39,6 @@ class Perceptron(object):
 	def predict(self, X):
 		pass
 
-	def initialize_weights(self, X):
-		pass
 	def score(self, X):
 		pass
 
