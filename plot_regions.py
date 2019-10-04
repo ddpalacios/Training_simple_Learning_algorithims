847bfdd43bf928d3e6c69db6a2eb198d16c8abaa
Daniel <palaciosdanieldario@gmail.com>
2019-10-03 22:17:10 -0500

    Code fix

--- plot_regions.py
+++ plot_regions.py
@@ -1,10 +1,17 @@
+import matplotlib
+matplotlib.rcParams["backend"] = "Agg"
+
+
 '''This plotting method is refrenced from 
     Python Machine Learning Book
     by Sebastian Raschka & Vahid Mirjalili
 '''
 from matplotlib.colors import ListedColormap
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+#matplotlib.use('TkAgg')
 def plot_decision_regions(X, y, classifier, resolution=0.02):
      # setup marker generator and color map
     colors = ('red', 'blue')
@@ -25,3 +32,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     plt.ylim(matrix2.min(), matrix2.max())
     for idx, cl in enumerate(np.unique(y)): #Graphs all points that have -1 and 1
         plt.scatter(X[y == cl, 0],  X[y == cl, 1],   alpha=0.6,   c=cmap(idx)    ,edgecolor='black')
+
+
+
+plt.show(block = True)
